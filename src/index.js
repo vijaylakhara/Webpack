b14c9fead1b96ce951bae9a97bd3fa4fb88feb23
DESKTOP-1I0VEAD\vijay kumar <vijaylakhara3.0@gmail.com>
2023-03-27 22:11:18 -0700

    comit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {other , other2} from "./second";
+
+console.log(other());
+other2();
---
+++ src/second.js
@@ -0,0 +1,7 @@
+export function  other(){
+    return "hello My Name is Kushal Kumar";
+}
+export function other2(){
+    document.write("Hello World")
+
+}
